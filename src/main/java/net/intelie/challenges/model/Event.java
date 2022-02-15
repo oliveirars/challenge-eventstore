8f812f43054f3b0784a32251f1025de3630d188d
Rodrigo Oliveira <rsilva.oliveira@gmail.com>
2022-02-14 21:34:44 -0300

    style: code formatting

--- src/main/java/net/intelie/challenges/model/Event.java
+++ src/main/java/net/intelie/challenges/model/Event.java
@@ -6,31 +6,31 @@ import java.util.Objects;
  * This is just an event stub, feel free to expand it if needed.
  */
 public class Event {
-    private final String type;
-    private final long timestamp;
+  private final String type;
+  private final long timestamp;
 
-    public Event(String type, long timestamp) {
-        this.type = type;
-        this.timestamp = timestamp;
-    }
+  public Event(String type, long timestamp) {
+    this.type = type;
+    this.timestamp = timestamp;
+  }
 
-    public String type() {
-        return type;
-    }
+  public String type() {
+    return type;
+  }
 
-    public long timestamp() {
-        return timestamp;
-    }
+  public long timestamp() {
+    return timestamp;
+  }
 
-    @Override
-    public boolean equals(Object obj) {
-      if (this == obj)
-        return true;
-      if (obj == null)
-        return false;
-      if (getClass() != obj.getClass())
-        return false;
-      Event other = (Event) obj;
-      return timestamp == other.timestamp && Objects.equals(type, other.type);
-    }
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Event other = (Event) obj;
+    return timestamp == other.timestamp && Objects.equals(type, other.type);
+  }
 }
